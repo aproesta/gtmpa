7e397d7d874f0c9d4d818e9a74c83a3e9bf670d2
aproesta <arthur.proestakis@gmail.com>
2015-11-16 10:54:17 +1100

    added manifest and some logic to calendar

--- src/main/webapp/scripts/app/entities/plan/plan-calendar.controller.js
+++ src/main/webapp/scripts/app/entities/plan/plan-calendar.controller.js
@@ -13,7 +13,28 @@ angular.module('gtmpaApp')
         });
         $scope.$on('$destroy', unsubscribe);
 
-        /* config object */
+        /**
+         * Calendar Events
+         */
+        var date = new Date();
+        var d = date.getDate();
+        var m = date.getMonth();
+        var y = date.getFullYear();
+
+        /* alert on eventClick */
+        $scope.alertOnEventClick = function( date, jsEvent, view){
+            $scope.alertMessage = (date.title + ' was clicked ');
+        };
+        /* alert on Drop */
+        $scope.alertOnDrop = function(event, delta, revertFunc, jsEvent, ui, view){
+            $scope.alertMessage = ('Event Droped to make dayDelta ' + delta);
+        };
+        /* alert on Resize */
+        $scope.alertOnResize = function(event, delta, revertFunc, jsEvent, ui, view ){
+            $scope.alertMessage = ('Event Resized to make dayDelta ' + delta);
+        };
+
+        /* calendar config object */
         $scope.uiConfig = {
             calendar:{
                 height: 450,
@@ -23,10 +44,23 @@ angular.module('gtmpaApp')
                     center: '',
                     right: 'today prev,next'
                 },
-                dayClick: $scope.alertEventOnClick,
+                dayClick: $scope.alertOnEventClick,
                 eventDrop: $scope.alertOnDrop,
                 eventResize: $scope.alertOnResize
             }
         };
+
+        /* Calendar Data */
+        /* event source that contains custom events on the scope */
+        $scope.events = [
+            {title: 'Initial Workshop',start: new Date(y, m, 2), allDay: true},
+            {title: 'Sales Competency',start: new Date(y, m, 7), allDay: true},
+            {title: 'PreSales Competency',start: new Date(y, m, 16), allDay: true},
+            {title: 'Technical Competency',start: new Date(y, m, 21), allDay: true},
+            {title: 'Solution Architecture',start: new Date(y, m +1 , 2), allDay: true},
+            {title: 'Solution Collateral', start: new Date(y, m + 1, 7), allDay: true}
+        ];
+
+        $scope.eventSources = [$scope.events];
 
     });
