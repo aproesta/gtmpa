347a312bf5f6f79eb9111798d329f761ce5c8d5d
Naresh Dimari <naresh.dimari@gmail.com>
2015-11-24 16:48:10 +1000

    Overdue highlight and filter

--- src/main/webapp/scripts/app/entities/action/action.controller.js
+++ src/main/webapp/scripts/app/entities/action/action.controller.js
@@ -4,16 +4,29 @@ angular.module('gtmpaApp')
     .controller('ActionController', function ($scope, $state, $modal, Action, ParseLinks) {
       
         $scope.actions = [];
+        $scope.currentDate = new Date();
+        $scope.currentDate.setHours(0, 0, 0, 0);
+        //window.alert("IAM ERE: " + $scope.currentDate);
         $scope.page = 0;
         $scope.loadAll = function() {
             Action.query({page: $scope.page, size: 20}, function(result, headers) {
                 $scope.links = ParseLinks.parse(headers('link'));
                 $scope.actions = result;
+               //Naresh Dimari this might interfare with creation of date widget in Action create page
+                angular.forEach( $scope.actions, function(action) {
+                	//window.alert("IAM ERE: " + action.nextActionDate); action.status=action.status+'overdue';
+              	  if((Date.parse(action.nextActionDate)) < $scope.currentDate.getTime()) {
+              		action.status=action.status+'overdue';
+              	  }
+              	});
             });
+            
+            
         };
         $scope.loadPage = function(page) {
             $scope.page = page;
             $scope.loadAll();
+            
         };
         $scope.loadAll();
 
@@ -29,7 +42,8 @@ angular.module('gtmpaApp')
                 description: null,
                 actionItems: null,
                 nextActionDate: null,
-                id: null
+                id: null,
+                status: null
             };
         };
     });
