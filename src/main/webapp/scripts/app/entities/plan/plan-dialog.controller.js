0d29b227b6d796be48482c8ea3e26ec8d0c5e53e
aproesta <arthur.proestakis@gmail.com>
2015-11-25 22:17:00 +1100

    update plan editor

--- src/main/webapp/scripts/app/entities/plan/plan-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/plan/plan-dialog.controller.js
@@ -68,4 +68,44 @@ angular.module('gtmpaApp').controller('PlanDialogController',
     		opened: false
     	};
     	
+    	/*
+    	 * Comments
+    	 */
+    	$scope.newComment = "";
+    	$scope.comments = "@david 2015-09-12 12:18 pm\nMeeting held to discuss planning approach.\n\n";
+    	$scope.comments = $scope.comments + "@arthur 2015-10-05 5:10 am\nNew actions documented to progress planning schedule.\n\n";
+    	$scope.comments = $scope.comments + "@peter 2015-11-07 2:30 pm\nExecutive sponsor David Smith now sponsoring solution.\n\n";
+    	
+    	/*
+    	 * History
+    	 */
+    	$scope.history = "@david 2015-09-12 12:18 pm\nSolution Name='WBS Solution' -> 'WAS Solution'.\n\n";
+    	$scope.history = $scope.history + "@arthur 2015-10-05 5:10 am\nProposal Date='2015-10-10' -> '2015-11-01'.\n\n";
+    	$scope.history = $scope.history + "@peter 2015-10-07 12:30 pm\nNew comment added.\n\n";
+    	$scope.history = $scope.history + "@anne 2015-10-17 4:30 pm\nComplete Date='2015-10-01' -> '2015-10-10.\n\n";
+
+    	/*
+    	 * Charts
+    	 */
+        $scope.c1_labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.c1_series = ['Partner A', 'Partner B'];
+        $scope.c1_data = [
+          [65, 59, 80, 81, 56, 55, 40],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+        $scope.c1_onClick = function (points, evt) {
+          console.log(points, evt);
+        };
+        
+        $scope.c2_labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+        $scope.c2_series = ['Partner A', 'Partner B'];
+
+        $scope.c2_data = [
+          [65, 59, 80, 81, 56, 55, 40],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+        
+        $scope.c3_labels = ["Health", "Government", "Manufacturing"];
+        $scope.c3_data = [300, 500, 100];
+
 }]);
