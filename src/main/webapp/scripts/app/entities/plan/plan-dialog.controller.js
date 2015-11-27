b010eee5c7aa884b06c50300fbd12494926d1cf0
aproesta <arthur.proestakis@gmail.com>
2015-11-28 08:29:53 +1100

    plan graphs

--- src/main/webapp/scripts/app/entities/plan/plan-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/plan/plan-dialog.controller.js
@@ -88,7 +88,7 @@ angular.module('gtmpaApp').controller('PlanDialogController',
     	 * Charts
     	 */
         $scope.c1_labels = ["January", "February", "March", "April", "May", "June", "July"];
-        $scope.c1_series = ['Partner A', 'Partner B'];
+        $scope.c1_series = ['Goal', 'Recognised'];
         $scope.c1_data = [
           [65, 59, 80, 81, 56, 55, 40],
           [28, 48, 40, 19, 86, 27, 90]
@@ -98,14 +98,14 @@ angular.module('gtmpaApp').controller('PlanDialogController',
         };
         
         $scope.c2_labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-        $scope.c2_series = ['Partner A', 'Partner B'];
+        $scope.c2_series = ['Solution', 'Market'];
 
         $scope.c2_data = [
           [65, 59, 80, 81, 56, 55, 40],
           [28, 48, 40, 19, 86, 27, 90]
         ];
         
-        $scope.c3_labels = ["Health", "Government", "Manufacturing"];
+        $scope.c3_labels = ["Telco1", "Telco2", "Telco3"];
         $scope.c3_data = [300, 500, 100];
 
 }]);
--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -6,26 +6,31 @@ angular.module('gtmpaApp')
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
             
-            $scope.c1_labels = ["January", "February", "March", "April", "May", "June", "July"];
-            $scope.c1_series = ['Partner A', 'Partner B'];
+            $scope.c1_labels = ["Q1", "Q2", "Q3", "Q4"];
+            $scope.c1_series = ['Cloud', 'Analytics', 'Mobile', 'Security', 'Social'];
             $scope.c1_data = [
-              [65, 59, 80, 81, 56, 55, 40],
-              [28, 48, 40, 19, 86, 27, 90]
+              [25, 50, 55, 65],
+              [80, 70, 70, 40],
+              [65, 59, 80, 81],
+              [15, 20, 45, 45],
+              [28, 48, 40, 19]
             ];
-            $scope.c1_onClick = function (points, evt) {
-              console.log(points, evt);
-            };
             
-            $scope.c2_labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-            $scope.c2_series = ['Partner A', 'Partner B'];
+            $scope.c2_labels = ['2013', '2014', '2015'];
+            $scope.c2_series = ['VIC', 'NSW', 'SA', 'WA', 'ACT', 'NT', 'TAS'];
 
             $scope.c2_data = [
-              [65, 59, 80, 81, 56, 55, 40],
-              [28, 48, 40, 19, 86, 27, 90]
+              [30, 59, 80, 11, 66],
+              [35, 48, 40, 19, 46],
+              [45, 59, 50, 21, 36],
+              [30, 65, 30, 39, 26],
+              [60, 65, 60, 31, 46],
+              [15, 70, 50, 49, 66],
+              [75, 59, 40, 31, 66]
             ];
             
-            $scope.c3_labels = ["Health", "Government", "Manufacturing"];
-            $scope.c3_data = [300, 500, 100];
+            $scope.c3_labels = ["Banking", "Health", "Government", "Manufacturing", "Transportation", "Retail"];
+            $scope.c3_data = [30, 50, 70, 100, 20, 79];
             
         });
     });
